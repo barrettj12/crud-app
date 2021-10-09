db90835ba2d14fd31bd680e2741de3a8ef5940eb
Jordan Barrett <90195985+barrettj12@users.noreply.github.com>
2021-10-09 14:42:01 +0200

    Add CORS header

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
-
+# Initiate Flask
+from flask import Flask, request, abort
+from flask.helpers import make_response
 app = Flask(__name__)
+
+# Constants
+ALLOWED_ORIGINS = {
+  "https://barrettj12.github.io/",
+  "http://127.0.0.1:5500"
+}
 
 
 @app.route('/test')
 def testResponse():
-  return 'Testing, testing, 1, 2, 3, ...'
+  # Check origin is allowed
+  if request.origin not in ALLOWED_ORIGINS:
+    abort(403, "Requests not allowed from your domain")
+
+  response = make_response('Testing, testing, 1, 2, 3, ...')
+  response.headers['Access-Control-Allow-Origin'] = request.origin
+  return response
