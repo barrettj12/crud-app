50dd0f8a4146d1cc1ed54e477e090b74b8f634f0
Jordan Barrett <90195985+barrettj12@users.noreply.github.com>
2021-11-06 19:11:20 +0100

    Add minimal test suite

---
+++ conftest.py
@@ -0,0 +1,36 @@
+#   CRUD APP
+#     https://github.com/barrettj12/crud-app
+#   Author: Jordan Mitchell Barrett (@barrettj12)
+#     https://github.com/barrettj12
+#
+#   /test/conftest.py
+#   Provide common fixtures for tests
+
+
+import pytest
+
+from app import create_app
+from app.helpers import initDB
+
+
+# Sets up a test client and initialises fresh local database
+@pytest.fixture
+def client():
+    # Import database address from /test/dbconfig.py
+    try:
+        from dbconfig import DATABASE_URL
+        print('Using database URL from /test/dbconfig.py')
+    except ImportError as e:
+        raise RuntimeError('No database URL has been provided for testing. To provide the database URL, create a file /test/dbconfig.py which defines a variable DATABASE_URL.') from e
+
+    print(f'Database URL is {DATABASE_URL}')
+
+
+    # Create app for testing
+    app = create_app(DATABASE_URL)
+
+    with app.test_client() as client:
+        with app.app_context():
+            # Set up database
+            initDB()
+            yield client
---
+++ test/init_test.py
@@ -0,0 +1,31 @@
+#   CRUD APP
+#     https://github.com/barrettj12/crud-app
+#   Author: Jordan Mitchell Barrett (@barrettj12)
+#     https://github.com/barrettj12
+#
+#   /test/init_test.py
+#   Tests for initialisation of app
+
+
+from app.helpers import dbWrap, getCols
+
+
+def test_db_init(client):
+    with dbWrap() as cur:
+        # Check that only one table 'tables' has been created
+        cur.execute(
+         """SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema='public';"""
+        )
+        assert cur.fetchall() == [('tables',)]
+
+        # Check columns of 'tables' are right
+        assert getCols('tables', cur) == ['id', 'name', 'pwd']
+
+        # Check data in 'tables' is right
+        cur.execute('SELECT * FROM tables;')
+        data = cur.fetchall()
+        assert data == [(1, 'tables', None)]
+
+        
